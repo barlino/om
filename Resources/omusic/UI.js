a72a0ca59f40695f58976913db04d28c81755640
Barlino Yeh <barlinoyeh@otiga.com>
2011-09-08 19:48:06 +0800

    delete unused files

--- Resources/omusic/UI.js
+++
@@ -1,6 +0,0 @@
-Ti.include('Otiga/UI/HeaderPullView.js');
-Ti.include('Otiga/UI/FooterPullView.js');
-
-//Public Module API
-exports.HeaderPullView = HeaderPullView;
-exports.FooterPullView = FooterPullView;
